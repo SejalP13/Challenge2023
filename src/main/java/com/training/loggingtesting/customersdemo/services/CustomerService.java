dfd04c9c7d5991a4da53f9ee5fb4872dcf41727c
Sejal Pagare <sejalpagare@gmail.com>
2023-08-11 06:22:37 +0000

    Changes done for Customer Entity

--- src/main/java/com/training/loggingtesting/customersdemo/services/CustomerService.java
+++ src/main/java/com/training/loggingtesting/customersdemo/services/CustomerService.java
@@ -1,11 +1,9 @@
-package com.training.loggingtesting.ordersdemo.services;
+package com.training.loggingtesting.customersdemo.services;
 
-import com.training.loggingtesting.ordersdemo.CountrySales;
-import com.training.loggingtesting.ordersdemo.Order;
-import com.training.loggingtesting.ordersdemo.repos.IOrderRepo;
-import com.training.loggingtesting.training.entities.MyMusic;
+import com.training.loggingtesting.customersdemo.CountrySales;
+import com.training.loggingtesting.customersdemo.Customer;
+import com.training.loggingtesting.customersdemo.repos.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,11 +13,11 @@ import java.util.List;
 public class OrderService {
 
     @Autowired
-    private IOrderRepo repo;
-    public List<Order> getAllOrders(){
+    private CustomerRepo repo;
+    public List<Customer> getAllOrders(){
         return  repo.findAll(); }
 
-    public Order getOrderById(String id){
+    public Customer getOrderById(String id){
         return repo.findById(id).get();
     }
 
@@ -27,7 +25,7 @@ public class OrderService {
         List<CountrySales> sales = new ArrayList<>();
         //AggregationResults<List<Order>> orders =  repo.getCountryWiseRevenue();
 
-        List<Order> orders =  repo.getCountryWiseRevenue();
+        List<Customer> orders =  repo.getCountryWiseRevenue();
        // orders.forEach((eList)->{
            orders.forEach(e->{
              CountrySales sale =  new CountrySales();
